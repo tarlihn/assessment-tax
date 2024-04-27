21be22dc98ba94a3d9010ce759b0788358a82b07
tarlihn <tarponset@gmail.com>
2024-04-27 11:24:27 +0700

    add test tax or refund

--- tax/calculation_test.go
+++ tax/calculation_test.go
@@ -2,58 +2,41 @@ package tax
 
 import (
 	"testing"
+
+	"github.com/tarlihn/assessment-tax/models"
 )
 
 func TestCalculateTax(t *testing.T) {
-	tests := []struct {
-		name        string
-		totalIncome float64
-		wht         float64
-		want        float64
-	}{
-		{
-			name:        "Income less than or equal to 150,000, no withholding tax",
-			totalIncome: 150000,
-			wht:         0,
-			want:        0,
-		},
-		{
-			name:        "Income between 150,001 and 500,000, no withholding tax",
-			totalIncome: 300000,
-			wht:         0,
-			want:        24000,
-		},
-		{
-			name:        "Income between 500,001 and 1,000,000, withholding tax applied",
-			totalIncome: 700000,
-			wht:         10000,
-			want:        55000,
-		},
-		{
-			name:        "Income between 1,000,001 and 2,000,000, withholding tax exceeds tax amount",
-			totalIncome: 1500000,
-			wht:         200000,
-			want:        0,
-		},
-		{
-			name:        "Income greater than 2,000,000, no withholding tax",
-			totalIncome: 2500000,
-			wht:         0,
-			want:        415000,
-		},
+	// Test case 1: Tax > 0
+	totalIncome1 := 500000.0
+	wht1 := 25000.0
+	response1, err1 := CalculateTax(totalIncome1, wht1)
+	if err1 != nil {
+		t.Errorf("Test case 1 failed: unexpected error: %v", err1)
+	}
+	textResponse1, ok1 := response1.(models.TextResponse)
+	if !ok1 {
+		t.Errorf("Test case 1 failed: expected TextResponse, got %T", response1)
+	}
+	expectedTax1 := 4000.0 // Expected tax: 500,000 (รายรับ) - 60,0000 (ค่าลดหย่อนส่วนตัว) = 440,000 tax 29,000.00 - 25,000.00 = 4,000
+	if textResponse1.Tax != expectedTax1 {
+		t.Errorf("Test case 1 failed: expected tax to be %f, got %f", expectedTax1, textResponse1.Tax)
 	}
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			got, err := CalculateTax(test.totalIncome, test.wht)
-
-			if err != nil {
-				t.Errorf("Unexpected error: %v", err)
-			}
-
-			if got != test.want {
-				t.Errorf("Incorrect tax calculation. Got: %v, Want: %f", got, test.want)
-			}
-		})
+	// Test case 2: Tax == 0
+	totalIncome2 := 150000.0
+	wht2 := 2000.0
+	response2, err2 := CalculateTax(totalIncome2, wht2)
+	if err2 != nil {
+		t.Errorf("Test case 2 failed: unexpected error: %v", err2)
 	}
+	refundResponse2, ok2 := response2.(models.RefundResponse)
+	if !ok2 {
+		t.Errorf("Test case 2 failed: expected RefundResponse, got %T", response2)
+	}
+	expectedRefund2 := 2000.0 // Expected refund: 150000*0. - 2000
+	if refundResponse2.Refund != expectedRefund2 {
+		t.Errorf("Test case 2 failed: expected refund to be %f, got %f", expectedRefund2, refundResponse2.Refund)
+	}
+
 }
