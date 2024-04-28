178280482cfa8dc5bab7b37345e1337547ab1024
tarlihn <tarponset@gmail.com>
2024-04-28 10:03:43 +0700

    add controller for recieve request

--- main.go
+++ main.go
@@ -1,29 +1,37 @@
 package main
 
 import (
-	"log"
-
-	"github.com/tarlihn/assessment-tax/database" // Import the database package
+	"github.com/tarlihn/assessment-tax/tax"
 
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	// Establish connection to the database
-	db, err := database.ConnectDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
 
-	// Database connection successful
-	log.Println("Connected to the database successfully!")
+	// // Establish connection to the database
+	// db, err := database.ConnectDB()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer db.Close()
+
+	// // Database connection successful
+	// log.Println("Connected to the database successfully!")
 
 	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
 	})
+	// tax := e.Group("/tax")
+	e.POST("/tax/calculation", tax.TaxController)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
---
+++ tax/controller.go
@@ -0,0 +1,22 @@
+package tax
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/tarlihn/assessment-tax/models"
+)
+
+func TaxController(c echo.Context) error {
+	req := new(models.RequestBody)
+	if err := c.Bind(req); err != nil {
+		return err
+	}
+
+	// Prepare response
+	result, _ := CalculateTax(req.TotalIncome, req.WHT, req.Allowances)
+
+	// Prepare response
+
+	return c.JSON(http.StatusOK, result)
+}
